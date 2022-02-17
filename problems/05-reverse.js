e54e7fa7c5d4c285ed608941da9550022d7b5d9a
chawin <pathocha000@gmail.com>
2022-02-16 19:57:33 -0800

    Solved #05

--- problems/05-reverse.js
+++ problems/05-reverse.js
@@ -12,12 +12,23 @@ reverse("id"); // "di"
 reverse(""); // ""
 ***********************************************************************/
 
+debugger
+
+// pop works for array
+// string is an array (of characters)
 
 function reverse(str) {
   // Your code here
+  if (str.length === 0) return "";
+  return str[str.length - 1] + reverse(str.slice(0, str.length - 1));
 }
 
-
+reverse("house"); // "esuoh"
+reverse("dog"); // "god"
+reverse("atom"); // "mota"
+reverse("q"); // "q"
+reverse("id"); // "di"
+reverse(""); // ""
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = reverse;
